3347494aaf889790636d9e486bd8789e2d866a34
David Montesdeoca <backpackerhh@gmail.com>
2024-02-27 10:05:22 +0100

    Improve test to ensure that all expected info about a repository is shown

--- src/components/repositories/RepositoriesSummary.test.js
+++ src/components/repositories/RepositoriesSummary.test.js
@@ -1,7 +1,7 @@
 import { screen, render } from "@testing-library/react";
 import RepositoriesSummary from "./RepositoriesSummary";
 
-test("displays the primary language of the repository", () => {
+test("displays info about the repository", () => {
   const repository = {
     language: "JavaScript",
     stargazers_count: 5,
@@ -11,7 +11,11 @@ test("displays the primary language of the repository", () => {
 
   render(<RepositoriesSummary repository={repository} />);
 
-  const language = screen.getByText("JavaScript");
+  for (const key in repository) {
+    const value = repository[key];
+    // const element = screen.getByText(value, { exact: false });
+    const element = screen.getByText(new RegExp(value));
 
-  expect(language).toBeInTheDocument();
+    expect(element).toBeInTheDocument();
+  }
 });
