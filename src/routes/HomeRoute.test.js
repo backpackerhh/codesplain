69892412b3990fe5a4077ab91897a27fec764ce4
David Montesdeoca <backpackerhh@gmail.com>
2024-02-27 17:20:20 +0100

    Extract all the boilerplate to use MSW to a test helper

--- src/routes/HomeRoute.test.js
+++ src/routes/HomeRoute.test.js
@@ -1,37 +1,23 @@
 import { render, screen } from "@testing-library/react";
-import { setupServer } from "msw/node";
-import { rest } from "msw";
 import { MemoryRouter } from "react-router-dom";
 import HomeRoute from "./HomeRoute";
+import { createServer } from "../testUtils/server";
 
-const handlers = [
-  rest.get("/api/repositories", (req, res, ctx) => {
-    const language = req.url.searchParams.get("q").split("language:")[1];
+createServer([
+  {
+    path: "/api/repositories",
+    res: (req) => {
+      const language = req.url.searchParams.get("q").split("language:")[1];
 
-    return res(
-      ctx.json({
+      return {
         items: [
           { id: 1, full_name: `${language}/a` },
           { id: 2, full_name: `${language}/b` },
         ],
-      })
-    );
-  }),
-];
-
-const server = setupServer(...handlers);
-
-beforeAll(() => {
-  server.listen();
-});
-
-afterEach(() => {
-  server.resetHandlers();
-});
-
-afterAll(() => {
-  server.close();
-});
+      };
+    },
+  },
+]);
 
 test("renders two links for each language", async () => {
   render(
