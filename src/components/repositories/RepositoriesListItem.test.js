d790dd6b5e2b475e8a7f381ebc02add2c7d96dda
David Montesdeoca <backpackerhh@gmail.com>
2024-02-27 12:52:21 +0100

    Add more tests for the repository item component

--- src/components/repositories/RepositoriesListItem.test.js
+++ src/components/repositories/RepositoriesListItem.test.js
@@ -2,18 +2,12 @@ import { screen, render } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import RepositoriesListItem from "./RepositoriesListItem";
 
-jest.mock("../tree/FileIcon", () => {
-  return () => {
-    return "File Icon Component";
-  };
-});
-
-test("shows a link to the GitHub homepage for a given repository", () => {
+function renderComponent() {
   const repository = {
     language: "JavaScript",
     full_name: "facebook/react",
     description: "React",
-    owner: "facebook",
+    owner: { login: "facebook" },
     name: "react",
     html_url: "https://github.com/facebook/react",
   };
@@ -24,9 +18,38 @@ test("shows a link to the GitHub homepage for a given repository", () => {
     </MemoryRouter>
   );
 
+  return { repository };
+}
+
+test("shows a link to the GitHub homepage for a given repository", async () => {
+  const { repository } = renderComponent();
+
+  // Note: the sign that the component has finished the state of fetching
+  await screen.findByRole("img", { name: "JavaScript" });
+
   const link = screen.getByRole("link", {
     name: /github repository/i,
   });
 
   expect(link).toHaveAttribute("href", repository.html_url);
+});
+
+test("shows a file icon with the appropriate icon", async () => {
+  renderComponent();
+
+  const icon = await screen.findByRole("img", { name: "JavaScript" });
+
+  expect(icon).toHaveClass("js-icon");
+});
+
+test("shows a link to the code editor page", async () => {
+  const { repository } = renderComponent();
+
+  await screen.findByRole("img", { name: "JavaScript" });
+
+  const link = screen.getByRole("link", {
+    name: new RegExp(repository.owner.login),
+  });
+
+  expect(link).toHaveAttribute("href", `/repositories/${repository.full_name}`);
 });
